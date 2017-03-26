fd4e0eab4fba45a28bb9dd9cd8228487421caed5
xtuhcy <xtuhcy@163.com>
2017-03-26 20:44:54 +0800

    发布1.2.8版本

--- src/main/java/com/geccocrawler/gecco/redis/RedisStartScheduler.java
+++ src/main/java/com/geccocrawler/gecco/redis/RedisStartScheduler.java
@@ -1,10 +1,6 @@
 package com.geccocrawler.gecco.redis;
 
 import org.redisson.Redisson;
-import org.redisson.api.RAtomicLong;
-import org.redisson.api.RBloomFilter;
-import org.redisson.api.RCountDownLatch;
-import org.redisson.api.RLock;
 import org.redisson.api.RScoredSortedSet;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -21,19 +17,6 @@ public class RedisStartScheduler implements Scheduler {
 		config.useSingleServer().setAddress(address);
 		RedissonClient redisson = Redisson.create(config);
 		set = redisson.getScoredSortedSet("Gecco-StartScheduler-Redis-ScoreSet");
-		
-		RBloomFilter<String> bf = redisson.getBloomFilter("bf");
-		bf.add("a");
-		RAtomicLong aLong = redisson.getAtomicLong("test");
-		aLong.getAndSet(2);
-		
-		RCountDownLatch rcdl = redisson.getCountDownLatch("cdl");
-		rcdl.countDown();
-		
-		RLock rlock = redisson.getLock("lock");
-		rlock.lock();
-		rlock.unlock();
-		
 	}
 
 	@Override
